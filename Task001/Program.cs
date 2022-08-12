c691aa03700c22499978255012fec999c38f4e33
ElenaG <freshelf@yandex.ru>
2022-08-12 14:42:38 +0500

    add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Palindrome(int number)
+{
+    if (number < 10000 && number > 99999)
+        Console.WriteLine("Введите пятизначное число");
+    else
+        {
+            if (number / 10000 == number % 10 && (number / 1000) % 10 == (number % 100) / 10)
+                Console.WriteLine($"{number} -> да");
+            else
+                Console.WriteLine($"{number} -> нет");
+        }
+}
+
+Palindrome(43625);
+Palindrome(57275);
+Palindrome(34);
+Palindrome(1234321); 
+
