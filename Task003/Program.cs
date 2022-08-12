be1c3c71683641570530d0f670a427264b552cbf
ElenaG <freshelf@yandex.ru>
2022-08-12 14:46:46 +0500

    Add Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void FindCubes(int number)
+{
+    Console.Write($"{number} -> ");
+
+    int count = 1;
+    while (count < number)
+    {
+        Console.Write($"{Math.Pow(count, 3)}, ");
+        count++;  
+    }
+Console.WriteLine($"{Math.Pow(number, 3)}.");
+}
+
+FindCubes(3);
+FindCubes(5);
